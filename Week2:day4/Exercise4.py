3b39cb0333e7cba73fbb89bf1847d361f8e6c8f6
BhavnaRambrichh <167183695+BhavnaRambrichh@users.noreply.github.com>
2024-05-21 04:30:18 +0400

    Delete Week2:day4 directory

--- Week2:day4/Exercise4.py
+++
@@ -1,2 +0,0 @@
-#Create a function that accepts a number between 1 and 100 and generates another number randomly between 1 and 100. Use the random module.
-#Compare the two numbers, if it’s the same number, display a success message, otherwise show a fail message and display both numbers.
