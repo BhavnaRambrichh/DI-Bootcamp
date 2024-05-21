0db77784d4afe16d4b3ea31afff62e10089a0180
BhavnaRambrichh <167183695+BhavnaRambrichh@users.noreply.github.com>
2024-05-21 04:29:32 +0400

    Delete Week2:day2 directory

--- Week2:day2/Exercise2.py
+++
@@ -1,4 +0,0 @@
-#Given a tuple which value is integers, is it possible to add more integers to the tuple?
-
-#Answer is No
-#as Tuple are immutable and do not change.
--- Week2:day2/Exercise4.py
+++
@@ -1,9 +0,0 @@
-#Recap – What is a float? What is the difference between an integer and a float?
-#Create a list containing the following sequence 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5 (don’t hard-code the sequence).
-#Can you think of another way to generate a sequence of floats?
-
-## Answer 1.Float are numbers with decimals 
-## Answer 2.Integers are whole numbers whereas float are both whole numbers with decimals.
-
-my_numbers = [1.5,2,2.5,3,3.5,4,4.5,5]
-
